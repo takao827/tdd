f99d05265be099e0c4cef9ef744fd36e9154f34f
takao827 <takao827.dds@gmail.com>
2026-07-07 05:19:05 +0000

    Dollarの副作用をなくす

--- tests/Unit/MoneyTest.php
+++ tests/Unit/MoneyTest.php
@@ -4,7 +4,10 @@ use App\Money\Dollar;
 
 test('multiplication', function () {
     $five = new Dollar(5);
-    $five->times(2);
 
-    expect($five->amount)->toBe(10);
+    $product = $five->times(2);
+    expect($product->amount)->toBe(10);
+
+    $product = $five->times(3);
+    expect($product->amount)->toBe(15);
 });

cdc37c8b2fe8dd1fbbf139d3f8b0518daa578410
takao827 <takao827.dds@gmail.com>
2026-07-07 05:46:50 +0000

    通貨にFrancを追加

---
+++ app/Money/Franc.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Money;
+
+class Franc
+{
+    private int $amount;
+
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier): Franc
+    {
+        return new Franc($this->amount * $multiplier);
+    }
+
+    public function equals(Franc $franc): bool
+    {
+        return $this->amount == $franc->amount;
+    }
+}
--- tests/Unit/MoneyTest.php
+++ tests/Unit/MoneyTest.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Money\Dollar;
+use App\Money\Franc;
 
 test('multiplication', function () {
     $five = new Dollar(5);
     expect($five->times(2))->toEqual(new Dollar(10));
     expect($five->times(3))->toEqual(new Dollar(15));
+});
+
+test('franc multiplication', function () {
+    $five = new Franc(5);
+    expect($five->times(2))->toEqual(new Franc(10));
+    expect($five->times(3))->toEqual(new Franc(15));
 });
 
 test('equality', function () {

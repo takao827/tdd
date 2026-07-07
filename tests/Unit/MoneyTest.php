981751da0227aa7806c2c939e527d9ee41fa40d5
takao827 <takao827.dds@gmail.com>
2026-07-07 07:23:16 +0000

    DollarとFrancの重複を排除

--- tests/Unit/MoneyTest.php
+++ tests/Unit/MoneyTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Money\Money;
-use App\Money\Franc;
 
 test('multiplication', function () {
     $five = Money::dollar(5);
@@ -9,22 +8,10 @@ test('multiplication', function () {
     expect($five->times(3)->equals(Money::dollar(15)))->toBeTrue();
 });
 
-test('franc multiplication', function () {
-    $five = Money::franc(5);
-    expect($five->times(2)->equals(Money::franc(10)))->toBeTrue();
-    expect($five->times(3)->equals(Money::franc(15)))->toBeTrue();
-});
-
 test('equality', function () {
     expect(Money::dollar(5)->equals(Money::dollar(5)))->toBeTrue();
     expect(Money::dollar(5)->equals(Money::dollar(6)))->toBeFalse();
-    expect(Money::franc(5)->equals(Money::franc(5)))->toBeTrue();
-    expect(Money::franc(5)->equals(Money::franc(6)))->toBeFalse();
     expect(Money::dollar(5)->equals(Money::franc(5)))->toBeFalse();
-});
-
-test('different class equality', function () {
-    expect(new Money(10, 'CHF')->equals(new Franc(10, 'CHF')))->toBeTrue();
 });
 
 test('currency', function () {

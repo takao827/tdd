c4a294a807c1e93f9d0bb456341cbcc9eacdd64d
takao827 <takao827.dds@gmail.com>
2026-07-14 08:19:31 +0000

    plusの抽出

--- app/Money/Expression.php
+++ app/Money/Expression.php
@@ -4,5 +4,6 @@ namespace App\Money;
 
 interface Expression
 {
+    public function plus(Expression $addend): Expression;
     public function reduce(Bank $bank, string $to): Money;
 }
--- app/Money/Sum.php
+++ app/Money/Sum.php
@@ -20,4 +20,9 @@ class Sum implements Expression
             + $this->addend->reduce($bank, $to)->amount();
         return new Money($amount, $to);
     }
+
+    public function plus(Expression $addend): Expression
+    {
+        return new Sum($this, $addend);; // TODO
+    }
 }
